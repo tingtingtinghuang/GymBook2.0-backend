a6c9d8b646b4654d6ef0d5448663243a9f70f15e
caicn <caicn@asiainfo.com>
2018-04-24 16:47:53 +0800

    提交route自动匹配

--- app/route.js
+++ app/route.js
@@ -11,48 +11,62 @@ const upload = multer({ storage: storage });
 
 const indexControl = require('./control/index');
 
-const indexRouteList = require('./route-list/index-router');
-const newsRouteList = require('./route-list/news-router');
+const indexRouteList = require('./route-list/index');
+
+let fs = require('fs');
+let path = require('path');
 
 module.exports = function (app, express, acl) {
-    app.use('/', cors());
-    app.use(cookieParser());
-    app.use(parseForm);
-    app.use(csrfProtection);// 所有的post请求都会验证csrf 具体参考 {@link https://www.npmjs.com/package/csurf}
+    let totalPath = path.join(__dirname, './route-list');
+    // 读取router-list文件下的路径，对应路径就是路由路径
+    fs.readdir(totalPath, (err, dirs) => {
+        if (err) throw err;
+        let routeList = dirs
+            .filter(val => val.includes('.js'))
+            .forEach(val => {
+                const func = require('./route-list/' + val);
+                const name = '/' + val.split('.js')[0];
+                const route = func({ upload });
+                app.use(name, route);
+            });
 
-    app.use((req, res, next) => {
-        res.locals.requestUrl = req.url;
-        res.locals.csrfToken = req.csrfToken();
-        next();
-    });
+        app.use('/', cors());
+        app.use(cookieParser());
+        app.use(parseForm);
+        app.use(csrfProtection);// 所有的post请求都会验证csrf 具体参考 {@link https://www.npmjs.com/package/csurf}
+
+        app.use((req, res, next) => {
+            res.locals.requestUrl = req.url;
+            res.locals.csrfToken = req.csrfToken();
+            next();
+        });
 
 
-    const indexRoute = indexRouteList({ upload });// index
-    const newsRoute = newsRouteList({ upload });// index
-    app.use(indexRoute);
-    app.use('/news', newsRoute);
+        const indexRoute = indexRouteList({ upload });// index
+        app.use(indexRoute);
 
-    // catch 404 and forward to error handler
-    app.use(function(req, res, next) {
-        const err = new Error('Not Found');
-        err.status = 404;
-        next(err);
-    });
+        // catch 404 and forward to error handler
+        app.use(function (req, res, next) {
+            const err = new Error('Not Found');
+            err.status = 404;
+            next(err);
+        });
 
-    // error handler
-    app.use(function(err, req, res, next) {
-        // set locals, only providing error in development
-        res.locals.message = err.message;
-        res.locals.error = req.app.get('env') === 'development' ? err : {};
+        // error handler
+        app.use(function (err, req, res, next) {
+            // set locals, only providing error in development
+            res.locals.message = err.message;
+            res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-        // render the error page
-        res.status(err.status || 500);
-        res.send({
-            code: -1,
-            msg: 'error',
-            data: err
-        }); // 替换为api返回
-        // web storm debug
-        // console.error('express catch error', err);
+            // render the error page
+            res.status(err.status || 500);
+            res.send({
+                code: -1,
+                msg: 'error',
+                data: err
+            }); // 替换为api返回
+            // web storm debug
+            // console.error('express catch error', err);
+        });
     });
 };
