1cfd4535e2e5a670fdb0898b98efd42cd765f211
soul <relsoul@outlook.com>
2018-04-28 12:41:51 +0800

    fix

--- app/control/news.js
+++ app/control/news.js
@@ -9,9 +9,6 @@ const { model: newsTypeModel } = require('../model/new_type');
 module.exports['index'] = async function (req, res) {
     let data = await newsModel.findAll();
     let query = {};
-    if(req.query.id){
-        query.id=parseInt(req.query.id);
-    }
     res.send({
         code: 1,
         msg: 'get news done',
