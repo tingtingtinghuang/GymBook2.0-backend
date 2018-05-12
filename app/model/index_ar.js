e2110f9a8ee1ee556a830c432d692b8ebd7081d4
wangfeiixang <871804591@qq.com>
2018-05-12 21:34:35 +0800

    添加时间计算的公共方法；查询gyms表，获取首页场地接口数据

--- app/model/index_ar.js
+++ app/model/index_ar.js
@@ -6,13 +6,15 @@ const sequelize = require('../../conf/conn');
 const Sequelize = require('sequelize');
 
 const conf = {
-    content: Sequelize.TEXT,    // 内容
-    startTime: Sequelize.STRING,    // 开始时间
+    content: Sequelize.TEXT,    // 内容 html
+    startTime: Sequelize.STRING,    // 开始时间 open_time
     endTime: Sequelize.STRING,    // 开始时间
     remainingTime: Sequelize.INTEGER,    // 剩余时间
 };
 
 const IndexAr = sequelize.define('index_ar', conf);
+
+
 
 module.exports = {
     conf,
--- app/model/sync/init-index_ar.js
+++ app/model/sync/init-index_ar.js
@@ -1,6 +1,41 @@
 const { model } = require('../index_ar');
+const sequelize = require('../../../conf/conn');
+let getDateDiff = require('../../unit/formatTime');
+
+// console.log("fun",getDateDiff("2016-03-28 10:30:22","2016-03-28 10:38:22","minute"))
+
+const Sequelize = require('sequelize');
 function init() {
-    model.sync({ force: true });
+
+    sequelize.query("SELECT * FROM gyms").spread(results => {
+        var results = results;
+        results = JSON.stringify(results);
+        results = JSON.parse(results);
+        // console.log(results);
+        results.forEach(ele => {
+            // console.log("first ele",ele)
+            for (let key in ele) {
+                if ( key == "html" ) {
+                   ele["content"] = ele["html"];
+                   delete ele["html"];
+                }
+
+                if ( key == "open_time" ) {
+                    console.log("time",ele.open_time,"end",ele.open_time.slice(18,36))
+                    ele["startTime"] = ele.open_time.slice(0,16);
+                    ele["endTime"] = ele.open_time.slice(18,36);
+                    ele["remainingTime"] = getDateDiff(ele.open_time.slice(0,16),ele.open_time.slice(18,36),"minute");
+                    delete ele["open_time"];
+                }
+
+
+               
+            }
+
+            // console.log("second",ele)
+        });
+
+    })
 }
 
 init();
