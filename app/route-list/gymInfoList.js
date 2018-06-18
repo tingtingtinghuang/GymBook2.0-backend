9cf16e886b65c7bbcc3d6f1b19b7890d09585d1d
huangtting <ishuangtingting@foxmail.com>
2018-06-18 22:28:22 +0800

    删除多余模块

--- app/route-list/gymInfoList.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ctrl = require('../control/gymInfoList');
-
-module.exports = function ({ auth, upload, app }) {
-    app.use('/gymInfoList', router);
-    router.get('/', ctrl.gymInfoList);
-    router.post('/', ctrl.gymInfoList);
-};
--- app/route-list/gymReserve.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ctrl = require('../control/gymReserve.js');
-
-module.exports = function ({ auth, upload, app }) {
-    app.use('/gymReserve', router);
-    router.get('/', ctrl.gymReserve);
-};
